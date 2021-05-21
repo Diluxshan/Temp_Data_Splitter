3fd9c1ee6e3261ca2c6ec76b2acad9067bc1a2e4
dilux <diluxv@gmail.com>
2021-05-21 15:40:51 +0530

    changing the file

--- CSV_writer.py
+++ CSV_writer.py
@@ -5,7 +5,7 @@ import datetime
 import dt as dt
 import pandas as pd
 
-with open("/home/dilux/SenzMate/Bogawanthalawa/Analyzing/report.csv", "r", encoding='utf-8-sig') as f:
+with open("/home/dilux/SenzMate/Bogawanthalawa/Analyzing/may_21.csv", "r", encoding='utf-8-sig') as f:
     reader = csv.DictReader(f)
     rows = [row for row in reader if row['Temp'] > '26.00']
 
@@ -17,8 +17,8 @@ for row in rows:
     arr_list.append(row)
 print(arr_list)
 
-with open("/home/dilux/SenzMate/Bogawanthalawa/Analyzing/output.csv","w",newline="") as f:
-    title = "Time,Temperature".split(",") # quick hack
+with open("/home/dilux/SenzMate/Bogawanthalawa/Analyzing/output_may21.csv","w",newline="") as f:
+    title = "Time,Temp".split(",") # quick hack
     cw = csv.DictWriter(f,title,delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
     cw.writeheader()
-    cw.writerows(data)
+    cw.writerows(arr_list)
